2ee7151f323a2b2b6200481b3ecc9cf66442b9a4
ravikiran1995 <rjravikiran95@gmail.com>
2020-07-19 19:17:48 +0530

    data types

---
+++ dataTypes.js
@@ -0,0 +1,65 @@
+// data Types in javascript
+
+/*
+what is data types?
+=> it defines the value type which is defined
+*/ 
+
+// let message = 'My Message';
+// console.log("Type of message:",typeof message);
+// // re-assinging the value with number
+// message = 20 ;// number
+// message = 32.56;// number
+// message = 23456789098765n;// bigInt
+// console.log("Type of message:",typeof message);
+
+// String declarations
+// let embeddedString = "My name";
+// let singleQuote = 'single quote string';
+// let doubleQuote = "double quote string";
+// let backTick = `backticks string ${embeddedString}`;// template string 
+// console.log(backTick);
+
+// function welcomeUser(name){
+//     let printName = "Hello "+name+ " welcome to class";
+//     console.log(printName); // without tempalte string
+//     console.log(`Hello ${name} welcome to Javascript class`);// with tempalte string
+// }
+// welcomeUser('Ambika');
+
+
+// Boolean 
+// let userLoggedIn = true;
+// let isGreater = false;
+// console.log(typeof userLoggedIn);
+
+// let age = null ;
+// let user ={name:'Ravi'};
+// console.log(typeof user);
+
+// undefined 
+// let age = undefined;// undefined is also a special data type in javascript
+// console.log(age);
+
+// function type 
+
+// function funType(){
+//     console.log("Hello");
+// }
+// console.log(typeof funType);
+
+// typeOf 
+// console.log(typeof undefined);//undefined
+// console.log(typeof 0);//number
+// console.log(typeof 'Hellow');// string
+// console.log(typeof 2324235423n);//bigInt
+// console.log(typeof "welcome");// string
+// console.log(typeof true);//boolean
+// console.log(typeof null);//object
+// console.log(typeof alert);//function type
+// console.log(typeof prompt());
+
+
+
+
+
