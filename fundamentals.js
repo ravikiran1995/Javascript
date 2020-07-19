29c872e9f2f14d9b135c52ae947d76d4f9a70617
ravikiran1995 <rjravikiran95@gmail.com>
2020-07-19 18:48:32 +0530

    Fundamentals

---
+++ fundamentals.js
@@ -0,0 +1,66 @@
+// variable in javascript
+
+/*
+What is Variable?
+=>A variable is a "named storage" for data.
+*/
+
+let message = "Hello world";
+let name = "Jhon",
+  age = 24;
+// var let const
+
+/*
+var: var is a funcitonal scope
+let and const :is a block scope
+*/
+
+// var foo = "MyViz Info";
+
+// function myFunc() {
+//   console.log(foo);
+// }
+// console.log(foo);
+// myFunc();
+
+// var a = 20;
+// console.log(a);
+// a = 30;
+// console.log(a);
+
+// Redefining the value with var
+// var a = 50;
+// function myFunc() {
+//   var a = 10;
+//   a = 100;
+//   var a = 200;
+//   console.log("func:", a);
+// }
+// myFunc();
+// console.log("out:", a);
+
+// Let keyword
+// let a =20;
+// console.log("initial",a);
+// a = 30;
+// console.log("re-assing",a);
+// let a = 50;// re-declaration is not allowed for let keword
+// console.log('re-defining',a);
+
+// const user = {
+//      name: "Ravi",
+//      email: "ravi@myviz.com"
+// }
+
+// console.log(user);
+// user.name = "Kiran";
+// console.log(user);
+
+// function letFunc() {
+//   let a = 100;
+//   console.log(a)
+// }
+// letFunc();
+// console.log(a);
+
+// const MY_NAME = "Ravi kiran";
