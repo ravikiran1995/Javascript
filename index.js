943ce5a19477146b19ef386aac9b240e7c7ec93e
ravikiran1995 <rjravikiran95@gmail.com>
2020-07-19 18:16:59 +0530

    hello world and code structure

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-// welcome to Javascript training
-var hello = "Hello Welcome to Javascript class";
+// Code style / Code structure
+
+// var hello = "Hello Welcome to Javascript class";
+
+// single line comment
+/*
+multiline comment
+*/ 
+
+let a = 20; 
+let b = 45;
+
+// naming conventions
+
+// camelCase
+// snake_case
+// PascalCase
+
+// var helloMessage = "Something new we are exploring today";
+
+// function camelCase(){
+
+// }
