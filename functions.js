98e8c214369ef3ac274553091786e1b522defacb
ravikiran1995 <rjravikiran95@gmail.com>
2020-07-26 17:09:18 +0530

    javascript function

---
+++ functions.js
@@ -0,0 +1,74 @@
+// functions
+
+/*
+1.Function declaration
+2.Function expressions
+*/
+
+// 1. Function declartion
+
+// function showMessage(name) {
+//   console.log("Welcome ",name);
+// }
+// showMessage('Gaythri');
+// showMessage('Ambika');
+
+// local variable
+// function displayMessage() {
+//   let message = "Welcome to Javascript";
+//   console.log(message);
+// }
+// displayMessage();
+
+// function displayMessage(message) {
+//   console.log(message);
+// }
+// displayMessage("Hello");
+
+// 2.Function Expression
+
+// let message = function() {
+//   console.log("Hello");
+// };
+// message();
+
+// function message() {
+
+// message();
+// printMessage();
+
+/*
+Function Declaration :a function , declared as a separate statement, in the code
+Function Expression :a fucntion , created inside an expression or inside another syntax construct
+*/
+
+// let message = function() {
+//   console.log("Helooooo");
+// };
+
+// message();
+
+// showMessage();
+
+// function showMessage(){
+//     console.log("Hello");
+// }
+
+// let age = 10;
+// let age_2 = 24;
+
+// let result = age + age_2;
+// let avgAge = result / 2;
+
+// function ageSum(age1, age2) {
+//   return age1 + age2;
+// }
+
+// function avgAge(age_total) {
+//   return age_total / 2;
+// }
+
+// let result = ageSum(24, 35);
+// let avg = avgAge(result);
+// let resultAvg = avgAge(ageSum(20, 30));
+// console.log("Total Age average", resultAvg);
