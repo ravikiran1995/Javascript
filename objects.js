0c5636730d34d62e09e7a6dddcb8d685a2c37462
ravikiran1995 <rjravikiran95@gmail.com>
2020-07-28 08:01:26 +0530

    objects

---
+++ objects.js
@@ -0,0 +1,85 @@
+// Objects
+
+// example
+// let obj = {
+//   name: "String",
+//   age: 23
+// };
+
+// creating object
+// let user = {};
+// let user1 = new Object();
+
+// literals and properties
+
+// let user = {
+//   name: "Ravi",
+//   age: 24
+// };
+
+// user.email = "ravi@gmail.com";
+
+// console.log("Name:", user.name);
+// console.log("Age:", user.age);
+
+// delete user.age;
+
+// console.log("Age:", user.age);
+// console.log("Email:", user.email);
+
+// const user = {
+//   name: "Kiran",
+//   age: 22,
+//   email: "ravi@gmail.com",
+//   "likes birds":true
+// };
+
+// user = "User name here";
+// user.age = 45;
+
+// square brackets
+// console.log("Age:", user.age);
+// console.log("Name:", user["name"]);
+// console.log("Birds",user["likes birds"])
+
+// make a car
+// function makeCar(carName, model, yearOfManufacture) {
+//   return {
+//     carName,
+//     model,
+//     yearOfManufacture
+//   };
+// }
+
+// let honda = makeCar("Honda City", 2020, 2019);
+// let maruti = makeCar("Baleno", 2019, 2019);
+
+// console.log("Car Name:", honda.carName);
+// console.log("YOM", maruti.model);
+
+// Property existence or not
+
+// let car ={
+//     model:2020,
+//     yom:2019,
+// }
+
+// let isCarName = "carName" in car;
+
+// console.log("CarName", "model" in car);
+
+// let user = {
+//   first_name: "Ravi",
+//   last_name: "Kiran",
+//   age: 24,
+//   mobile: 9999999999,
+//   email: "ravi@gmail.com",
+//   location: "Bengaluru",
+//   dob: "03 Jan 2020",
+//   qualification: ["Degree","Masters","Phd"],
+//   isMarried: false
+// };
+
+// for (key in user) {
+//   console.log(key + " = " + user[key]);
+// }
